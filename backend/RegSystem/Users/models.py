d7d35cb66c9b01b3d12a67cb19148df54daa175f
demerzel-iv <demerzel_iv@qq.com>
2020-11-15 23:14:04 +0800

    修改User/model

--- backend/RegSystem/Users/models.py
+++ backend/RegSystem/Users/models.py
@@ -6,7 +6,6 @@ from django.db import models
 class user(models.Model):
     teamname = models.CharField(max_length=20)
     password = models.CharField(max_length=16, null=False, blank=False)
-    email = models.CharField(max_length=30, default='')
     type = models.CharField(max_length=1, default='')
     isAdmin = models.BooleanField(default=False)
 
@@ -15,6 +14,9 @@ class member(models.Model):
     name = models.CharField(max_length=10)
     school = models.CharField(max_length=20)
     gender = models.CharField(max_length=10)
+    email = models.CharField(max_length=30, default='')
+    phone = models.CharField(max_length=30, default='')
+    localtion = models.CharField(max_length=30, default='')
 
 
 class user2member(models.Model):
