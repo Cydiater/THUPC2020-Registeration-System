f013edcba780db21f73b2d1aafb18b9ee43742c0
焦景辉 <2019013253@secoder.net>
2020-11-22 17:45:09 +0800

    fix: use online db for debug

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -5,11 +5,13 @@ module.exports = {
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8000/',
+        target: 'http://62.234.43.40/',
+        //target: 'http://localhost:8000/',
         changeOrigin: true,
       },
       'auth': {
-        target: 'http://localhost:8000/',
+        //target: 'http://localhost:8000/',
+        target: 'http://62.234.43.40/',
         changeOrigin: true,
       }
     }
